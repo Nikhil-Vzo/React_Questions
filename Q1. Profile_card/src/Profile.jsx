1e3a706c170699900151454d199554add2117ab5
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-01-09 22:42:09 +0530

    did the task  till context of my knowledge

--- Q1. Profile_card/src/Profile.jsx
+++ Q1. Profile_card/src/Profile.jsx
@@ -1,17 +1,12 @@
-import './Profile.css'
 function Profile({ name, age, isOnline })
 {
     return(
-        <>
-        <div className="baap">
         <div className="card">
             <div className="pic"></div>
         <h1>Name: {name}</h1>
         <h1>Age: {age}</h1>
         <h1>Status: {isOnline ? "💚Online" : "❤️Offline"}</h1>
         </div>
-        </div>
-        </>
     )  
 }
 
--- Q1. Profile_card/src/UserData.jsx
+++ Q1. Profile_card/src/UserData.jsx
@@ -1,13 +1,14 @@
-import Profile from './Profile'
+import Profile from './Profile';
+import './Profile.css';
 
 function UserData()
 {
     return(
-        <>
-        <Profile name="Kartik" age={18} isOnline={true} />
-        <Profile name="Anjali" age={17} isOnline={false} />
-        <Profile name="Rohan" age={19} isOnline={true} />
-        </>
+        <div className="baap">
+            <Profile name="Kartik" age={18} isOnline={true} />
+            <Profile name="Anjali" age={17} isOnline={false} />
+            <Profile name="Rohan" age={19} isOnline={true} />
+        </div>
     )
 }
 
