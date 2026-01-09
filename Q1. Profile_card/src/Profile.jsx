7a634d049667e0d0b5b32b736282b278141af135
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-01-09 22:19:11 +0530

    used css

--- Q1. Profile_card/src/Profile.jsx
+++ Q1. Profile_card/src/Profile.jsx
@@ -1,10 +1,13 @@
+import './Profile.css'
 function Profile({ name, age, isOnline })
 {
     return(
         <>
+        <div className="card">
         <h1>Name: {name}</h1>
         <h1>Age: {age}</h1>
         <h1>Status: {isOnline ? "💚Online" : "❤️Offline"}</h1>
+        </div>
         </>
     )  
 }
