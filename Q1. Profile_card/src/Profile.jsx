c301ce44fbf9d21bc6e402e9ab4c894458705b55
Nikhil Yadav <nikhilyadav200530@gmail.com>
2026-01-09 22:35:17 +0530

    used css to restyle

--- Q1. Profile_card/src/Profile.jsx
+++ Q1. Profile_card/src/Profile.jsx
@@ -3,10 +3,13 @@ function Profile({ name, age, isOnline })
 {
     return(
         <>
+        <div className="baap">
         <div className="card">
+            <div className="pic"></div>
         <h1>Name: {name}</h1>
         <h1>Age: {age}</h1>
         <h1>Status: {isOnline ? "💚Online" : "❤️Offline"}</h1>
+        </div>
         </div>
         </>
     )  
